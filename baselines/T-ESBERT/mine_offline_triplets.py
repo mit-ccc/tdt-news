7ea0ce8834683e50893c119da672697118675965
Hang Jiang <hjian42@icloud.com>
2021-06-28 20:52:39 +0000

    add/offline triplet mining

---
+++ baselines/T-ESBERT/mine_offline_triplets.py
@@ -0,0 +1,89 @@
+import re
+import argparse
+import torch
+import os, pickle
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+parser = argparse.ArgumentParser(description="main training script for word2vec dynamic word embeddings...")
+parser.add_argument("--input_folder", type=str, default="./output/exp_time_esbert_ep2_mgn2.0_btch8_norm1.0_max_seq_128/", help="vector_path")
+args = parser.parse_args()
+
+def extract_triplets(train_dev_distance_mat, class2idx, idx2class):
+    EPHN_triplets = []
+    EPEN_triplets = []
+    HPHN_triplets = []
+    HPEN_triplets = []
+
+    threshold = np.percentile(train_dev_distance_mat, 99) # statistical test
+
+    # cosine disance in the range: [0, 2]
+    for idx, embed in enumerate(train_dev_distance_mat):
+
+        if idx % 1000 == 0:
+            print("Finsihed {} instances".format(idx))
+        
+        embed = np.array(embed) # make a copy
+        cluster = idx2class[idx]
+        
+        positive_idx_list = list(class2idx[cluster])
+        mask_pos = np.zeros(embed.shape,dtype=bool)
+        mask_pos[positive_idx_list] = True
+        mask_not_outlier = (embed < threshold)
+        negative_idx_list = list((mask_not_outlier * (~mask_pos)).nonzero())[0]
+        # print(negative_idx_list)
+        
+        sorted_pos_idx_list = np.argsort(embed[positive_idx_list], axis=0)
+        sorted_neg_idx_list = np.argsort(embed[negative_idx_list], axis=0)
+        
+        easy_pos = positive_idx_list[sorted_pos_idx_list[1]] # skip the first, itself
+        hard_pos = positive_idx_list[sorted_pos_idx_list[-1]]
+        easy_neg = negative_idx_list[sorted_neg_idx_list[0]]
+        hard_neg = negative_idx_list[sorted_neg_idx_list[-1]]
+        
+        EPHN_triplets.append((idx, easy_pos, hard_neg))
+        EPEN_triplets.append((idx, easy_pos, easy_neg))
+        HPHN_triplets.append((idx, hard_pos, hard_neg))
+        HPEN_triplets.append((idx, hard_pos, easy_neg))
+
+    return {
+            "EPHN_triplets": EPHN_triplets,
+            "EPEN_triplets": EPEN_triplets,
+            "HPHN_triplets": HPHN_triplets,
+            "HPEN_triplets": HPEN_triplets
+        }
+
+
+def main():
+
+    # train
+    with open('/mas/u/hjian42/tdt-twitter/baselines/T-ESBERT/dataset/train_dev.pickle', 'rb') as handle:
+        train_dev_corpus = pickle.load(handle)
+    print("finished loading train pickle files")
+
+    train_dense_feats = torch.load(os.path.join(args.input_folder, "train_sent_embeds.pt"))
+    train_dev_distance_mat = 1 - cosine_similarity(train_dense_feats, train_dense_feats)
+
+    # class2idx and idx2class
+    class2idx = {}
+    idx2class = {}
+    for idx, doc in enumerate(train_dev_corpus.documents):
+    #     docid = doc['id']
+        cluster = doc['cluster']
+        
+        idx2class[idx] = cluster
+        if cluster in class2idx:
+            class2idx[cluster].add(idx)
+        else:
+            class2idx[cluster] = set([idx])
+
+    # extract triplets
+    offline_triplets = extract_triplets(train_dev_distance_mat, class2idx, idx2class)
+    print("Finished extracting triplets...")
+    
+    with open(os.path.join(args.input_folder, "train_dev_offline_triplets.pickle"), 'wb') as handle:
+        pickle.dump(offline_triplets, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == "__main__":
+    main()
---
+++ baselines/T-ESBERT/tune_offline_pipeline.sh
@@ -0,0 +1,6 @@
+# extract offline triplets
+# python mine_offline_triplets.py --input_folder ./output/exp_sbert_ep1_mgn2.0_btch64_norm1.0_max_seq_128
+# python mine_offline_triplets.py --input_folder ./output/exp_esbert_ep7_mgn2.0_btch64_norm1.0_max_seq_128
+# python mine_offline_triplets.py --input_folder ./output/exp_time_esbert_ep3_mgn2.0_btch64_norm1.0_max_seq_128_fuse_selfatt_pool_random_sample_BatchHardTripletLoss
+# python mine_offline_triplets.py --input_folder ./output/exp_time_esbert_ep2_mgn2.0_btch64_norm1.0_max_seq_128_fuse_selfatt_pool_random_sample_BatchHardTripletLoss_time_frozen
+
