92df05231f5be320b7832d9de7f5cd4f63d76855
hjian42 <hjian42@icloud.com>
2021-08-11 04:21:46 +0000

    add/preprocessing

--- baselines/T-ESBERT/preprocessing/extract_entities_vaccine.py
+++ baselines/T-ESBERT/preprocessing/extract_entities_vaccine.py
@@ -5,13 +5,15 @@ import sys
 import tokenizations 
 from transformers import BertTokenizer
 
-sys.path.insert(0,"/mas/u/hjian42/tdt-twitter/baselines/T-ESBERT/")
 
-with open('/mas/u/hjian42/tdt-twitter/baselines/T-ESBERT/dataset/train_dev.pickle', 'rb') as handle:
+sys.path.insert(0,"/mas/u/hjian42/tdt-twitter/baselines/T-ESBERT/")
+class CorpusClass:
+    def __init__(self, documents):
+        self.documents = documents
+
+with open('/mas/u/hjian42/tdt-twitter/baselines/T-ESBERT/news_data/train_dev.pickle', 'rb') as handle:
     train_dev_corpus = pickle.load(handle)
 
-with open('/mas/u/hjian42/tdt-twitter/baselines/T-ESBERT/dataset/test.pickle', 'rb') as handle:
-    test_corpus = pickle.load(handle)
 
 nlp = spacy.load("en_core_web_md", disable=["lemmatizer"])
 bert_tokenizer = BertTokenizer.from_pretrained("sentence-transformers/bert-base-nli-mean-tokens")
@@ -39,7 +41,7 @@ def annotate_ner(nlp_model, corpus):
 
     texts = []
     for example in corpus.documents:
-        example['full_text'] = example['title'] + " . " + example['text']
+        # example['full_text'] = example['title'] + " . " + example['text']
         texts.append(example['full_text'])
     docs = list(nlp_model.pipe(texts, disable=["tagger", "parser"]))
 
@@ -68,12 +70,12 @@ train_dev_corpus = annotate_ner(nlp, train_dev_corpus)
 train_dev_corpus = bert_tokenize(bert_tokenizer, train_dev_corpus)
 train_dev_corpus = align_tokenizations(train_dev_corpus)
 
-with open('./train_dev.pickle', 'wb') as handle:
+with open('../news_data/train_dev_entity.pickle', 'wb') as handle:
     pickle.dump(train_dev_corpus, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-test_corpus = annotate_ner(nlp, test_corpus)
-test_corpus = bert_tokenize(bert_tokenizer, test_corpus)
-test_corpus = align_tokenizations(test_corpus)
+# test_corpus = annotate_ner(nlp, test_corpus)
+# test_corpus = bert_tokenize(bert_tokenizer, test_corpus)
+# test_corpus = align_tokenizations(test_corpus)
 
-with open('./test.pickle', 'wb') as handle:
-    pickle.dump(test_corpus, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# with open('./test.pickle', 'wb') as handle:
+#     pickle.dump(test_corpus, handle, protocol=pickle.HIGHEST_PROTOCOL)
